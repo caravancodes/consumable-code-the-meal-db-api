d5a6bcb12f242bba24db842b9c7585de70a95c6b
amirisback <faisalamircs@gmail.com>
2020-03-15 02:13:41 +0700

    add: Kotlin File

---
+++ frogomealsapi/src/main/java/com/frogobox/frogomealsapi/base/BaseMealDataSource.kt
@@ -0,0 +1,19 @@
+package com.frogobox.frogomealsapi.base
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * consumable-code-the-meal-db-api
+ * Copyright (C) 15/03/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.frogomealsapi.base
+ *
+ */
---
+++ frogomealsapi/src/main/java/com/frogobox/frogomealsapi/data/source/MealDataSource.kt
@@ -0,0 +1,19 @@
+package com.frogobox.frogomealsapi.data.source
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * consumable-code-the-meal-db-api
+ * Copyright (C) 15/03/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.frogomealsapi.data.source
+ *
+ */
