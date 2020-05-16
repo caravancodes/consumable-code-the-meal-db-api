a6a25a65f0e2981dd3c0ae0bc5df8aa79480935b
amirisback <faisalamircs@gmail.com>
2020-05-16 20:56:18 +0700

    add: sample code

--- app/src/main/java/com/frogobox/themealsapi/MainActivity.kt
+++ app/src/main/java/com/frogobox/themealsapi/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.frogobox.themealsapi
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.frogobox.frogothemealdbapi.ConsumeTheMealDbApi
 import com.frogobox.frogothemealdbapi.callback.MealResultCallback
-import com.frogobox.frogothemealdbapi.data.model.Category
+import com.frogobox.frogothemealdbapi.data.model.Meal
 import com.frogobox.frogothemealdbapi.data.response.MealResponse
+import com.frogobox.recycler.boilerplate.viewrclass.FrogoViewAdapterCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupConsumableMealApi("b")
+    }
 
-        val consumeMealApi = ConsumeTheMealDbApi("1")
+    private fun consumeMealApi(): ConsumeTheMealDbApi {
+        val consumeCode = ConsumeTheMealDbApi("1")
+        consumeCode.usingChuckInterceptor(this)
+        return consumeCode
+    }
 
-        consumeMealApi.usingChuckInterceptor(this)
-        consumeMealApi.listAllCateories(object : MealResultCallback<MealResponse<Category>> {
-            override fun getResultData(data: MealResponse<Category>) {
-                for (i in data.meals!!.indices) {
-                    Toast.makeText(this@MainActivity, data.meals!![i].strCategory, Toast.LENGTH_SHORT).show()
-                }
+    private fun setupConsumableMealApi(firstLetter: String) {
+        consumeMealApi().listAllMeal(firstLetter, object : MealResultCallback<MealResponse<Meal>> {
+            override fun getResultData(data: MealResponse<Meal>) {
+                data.meals?.let { setupFrogoRecyclerView(it) }
             }
 
             override fun failedResult(statusCode: Int, errorMessage: String?) {
@@ -30,13 +40,44 @@ class MainActivity : AppCompatActivity() {
 
             override fun onShowProgress() {
                 // Show Your Progress View
+                runOnUiThread {
+                    progress_bar.visibility = View.VISIBLE
+                }
             }
 
             override fun onHideProgress() {
                 // Hide Your Progress View
+                runOnUiThread {
+                    progress_bar.visibility = View.GONE
+                }
             }
         })
-
-
     }
+
+    private fun setupFrogoRecyclerView(data: List<Meal>) {
+        frogo_rv.injector<Meal>()
+            .addData(data)
+            .addCustomView(R.layout.frogo_rv_grid_type_13)
+            .addCallback(object : FrogoViewAdapterCallback<Meal> {
+                override fun onItemClicked(data: Meal) {
+                }
+
+                override fun onItemLongClicked(data: Meal) {
+
+                }
+
+                override fun setupInitComponent(view: View, data: Meal) {
+                    val ivPoster = view.findViewById<ImageView>(R.id.frogo_rv_type_13_iv_poster)
+                    val tvTitle = view.findViewById<TextView>(R.id.frogo_rv_type_13_tv_title)
+                    val tvSubTitle = view.findViewById<TextView>(R.id.frogo_rv_type_13_tv_subtitle)
+
+                    Glide.with(view.context).load(data.strMealThumb).into(ivPoster)
+                    tvTitle.text = data.strMeal
+                    tvSubTitle.text = data.strCategory
+                }
+            })
+            .createLayoutGrid(2)
+            .build()
+    }
+
 }
